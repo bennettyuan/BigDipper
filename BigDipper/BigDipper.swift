a07774da6144b7d28b7ca743a53839bb613a1b86
Bennett Yuan <bennettyuan18@gmail.com>
2021-07-07 17:03:03 +0800

    Add router type

--- BigDipper/BigDipper.swift
+++ BigDipper/BigDipper.swift
@@ -22,3 +22,38 @@ public protocol PresenterType: AnyObject {}
 public protocol WireframeType: AnyObject {}
 
 public protocol InteractorType: AnyObject {}
+
+public protocol RouterType: AnyObject {
+
+	func push(_ viewController: UIViewController, from source: UIViewController, animated: Bool)
+	func pop(_ viewController: UIViewController, animated: Bool)
+	func present(_ viewController: UIViewController, from source: UIViewController, animated: Bool, completion: (() -> Void)?)
+	func dismiss(_ viewController: UIViewController, animated: Bool, completion: (() -> Void)?)
+}
+
+extension RouterType {
+
+	func push(_ viewController: UIViewController, from source: UIViewController, animated: Bool = true) {
+		guard let sourceNavigationController = source.navigationController else {
+			fatalError("Router (\(self)) can't push view controller (\(viewController)), because of source view controller (\(source)) doesn't have navigation controller.")
+		}
+
+		sourceNavigationController.pushViewController(viewController, animated: animated)
+	}
+
+	func pop(_ viewController: UIViewController, animated: Bool = true) {
+		guard let navigationController = viewController.navigationController else {
+			fatalError("Router (\(self)) can't pop view controller (\(viewController)), because of it doesn't have navigation controller.")
+		}
+
+		navigationController.popViewController(animated: animated)
+	}
+
+	func present(_ viewController: UIViewController, from source: UIViewController, animated: Bool = true, completion: (() -> Void)? = nil) {
+		source.present(viewController, animated: animated, completion: completion)
+	}
+
+	func dismiss(_ viewController: UIViewController, animated: Bool = true, completion: (() -> Void)? = nil) {
+		viewController.dismiss(animated: animated, completion: completion)
+	}
+}
