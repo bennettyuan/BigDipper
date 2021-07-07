5b5e4a6df52646671b25032e91a9d62b3a675062
Bennett Yuan <bennettyuan18@gmail.com>
2021-07-07 18:16:22 +0800

    Fix typo

--- Templates/install.sh
+++ Templates/install.sh
@@ -3,7 +3,7 @@
 XCODE_TEMPLATE_DIR=$HOME'/Library/Developer/Xcode/Templates/File Templates/BigDipper'
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-install_xcode_templetes() {
+install_xcode_templates() {
   if [ -d "$XCODE_TEMPLATE_DIR" ]; then
     rm -R "$XCODE_TEMPLATE_DIR"
   fi
@@ -12,6 +12,6 @@ install_xcode_templetes() {
   cp -R $SCRIPT_DIR/*.xctemplate "$XCODE_TEMPLATE_DIR"
 }
 
-install_xcode_templetes
+install_xcode_templates
 
 echo "BigDipper have been set up. In Xcode, select 'New File...' to use BigDipper templates."
